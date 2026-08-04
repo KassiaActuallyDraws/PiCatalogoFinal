05db51a806c85f4e591c94d5e49b52d3b8977f65
Jose <joseluisrojassantos25@gmail.com>
2026-08-04 11:57:51 -0500

    stock y proveedor eliminados

--- DuckEEAPI/obj/Debug/net10.0/DuckEEAPI.AssemblyInfo.cs
+++ DuckEEAPI/obj/Debug/net10.0/DuckEEAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DuckEEAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d1f22041b18980dc294bbf76bf859e9001fb63d5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+11a11c5f8c8cc30cd4b536ad5de06fd7f77a75f7")]
 [assembly: System.Reflection.AssemblyProductAttribute("DuckEEAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DuckEEAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
