55b45ddc708cc74e264613ae3a61ad5176bc6032
KassiaActuallyDraws <kikiramez@gmail.com>
2026-08-05 09:03:28 -0500

    Ya funcionan las categorias (Solo faltan los datos)

---
+++ js/catalogo_figura.js
@@ -0,0 +1,71 @@
+const API="http://localhost:5129/api";
+
+const contenedor=document.getElementById("contenedorProductos");
+
+let buscador="Figura";
+
+let productos=[];
+
+setTimeout(categoria, 1000);
+
+document.addEventListener("DOMContentLoaded",()=>{
+
+    cargarProductos();
+
+});
+
+async function cargarProductos(){
+
+    const respuesta=await fetch(`${API}/productos`);
+
+    productos=await respuesta.json();
+
+    pintar(productos);
+
+}
+
+function pintar(lista){
+
+    contenedor.innerHTML="";
+
+    lista.forEach(p=>{
+
+        contenedor.innerHTML+=`
+
+        <div class="producto">
+
+            <a href="dato${p.id}.html">
+
+                <img src="${p.imagen}">
+
+            </a>
+
+            <p>${p.nombre}</p>
+
+            <span>$${Number(p.precio).toFixed(2)}</span>
+
+        </div>
+
+        `;
+
+    });
+
+}
+
+function categoria () {
+
+    const texto=buscador.toLowerCase();
+
+    const filtrados=productos.filter(p=>
+
+        p.nombre.toLowerCase().includes(texto)
+
+        ||
+
+        p.categoria.toLowerCase().includes(texto)
+
+    );
+
+    pintar(filtrados);
+
+};
